b415a888f6383c5c74d5b058bbd2ced863022299
Sam Dickinson <30512008+JazzXP@users.noreply.github.com>
2026-01-20 10:48:40 +1100

    refactor: Treesitter setup in init.lua
    Removed Treesitter configuration options and set lazy loading to false.

--- init.lua
+++ init.lua
@@ -406,18 +406,7 @@ require('lazy').setup(
     { -- Highlight, edit, and navigate code
       'nvim-treesitter/nvim-treesitter',
       build = ':TSUpdate',
-      main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-      opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-      },
+      lazy = false,
     },
     {
       "folke/persistence.nvim",
